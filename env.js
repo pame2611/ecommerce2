635d5189a4daf1fa842d0035d13431783aeb5c7f
Pamela <pame2611@gmail.com>
2023-02-28 02:50:07 -0300

    Inicia el  manejo de variables de entorno

---
+++ env.js
@@ -0,0 +1,1 @@
+export const API_URL= "https://api-ecommerce.ed.team/api/v1"
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import { API_URL } from "../env"
 function App() {
 
   return (
     <div>
-      <div>Hola mundo</div>
+      <div>Hola mundo{API_URL}</div>
     </div>
   )
 }
